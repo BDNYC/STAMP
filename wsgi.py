7395ac1da193a23586eaa5cb91849000813ce7a4
anthonymunozperez <munozanthony196@gmail.com>
2026-03-20 22:07:45 -0400

    Make server env vars survive Git redeploys
    Load /var/www/webroot/env.py (GRIDS_DIR, DEMO_DATA_DIR) automatically
    on the Reclaim Cloud server. Does nothing locally since the file
    doesn't exist.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
 import sys
 sys.stderr.write(f"WSGI USING: {sys.executable}\n"); sys.stderr.flush()
 
+# Load server environment variables (GRIDS_DIR, DEMO_DATA_DIR) if deployed
+try:
+    sys.path.insert(0, "/var/www/webroot")
+    import env  # noqa: F401
+except ImportError:
+    pass
+
 from app import app as application
